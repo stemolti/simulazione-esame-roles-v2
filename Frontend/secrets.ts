07c7a0d01632282f54ed5e1b78ba0949aae33ba4
Stefano Agbodan <mystefano95@gmail.com>
2025-07-12 00:12:55 +0200

    Update secrets.ts

--- Frontend/secrets.ts
+++ Frontend/secrets.ts
@@ -1,2 +1,1 @@
-export const apiUrl = 'http://localhost:3000/api';
-//export const apiUrl = 'https://simulazione-esame.vercel.app/api';
+export const apiUrl = 'https://simulazione-esame-roles-v2-xkc6.vercel.app/api';
